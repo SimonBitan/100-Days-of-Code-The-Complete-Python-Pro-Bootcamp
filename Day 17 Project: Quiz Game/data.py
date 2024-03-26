943b14da193cd62b22e03552f1b0eb2ac2654301
SimonBitan <133679189+SimonBitan@users.noreply.github.com>
2024-03-26 15:25:52 -0400

    Create data.py

---
+++ Day 17 Project: Quiz Game/data.py
@@ -0,0 +1,51 @@
+# question_data = [
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+#     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+#     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+#     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+#              "you are free to take it home to eat.", "answer": "True"},
+#     {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
+#      "answer": "False"},
+#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+#     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
+
+question_data = [
+    {"type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+     "question": "The pickled gherkin was first added to hamburgers because"
+                 " a US health law required all fast-food to include a source of Vitamin C.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science &amp; Nature",
+     "question": "Centripedal force is an apparent force that acts outward on a body moving around a center, "
+                 "arising from the body&#039;s inertia.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "multiple", "difficulty": "easy", "category": "Entertainment: Film",
+     "question": "Which of these actors\/actresses is NOT a part of the cast "
+                 "for the 2016 movie &quot;Suicide Squad&quot;?",
+     "correct_answer": "Scarlett Johansson", "incorrect_answers": ["Jared Leto", "Will Smith", "Margot Robbie"]},
+    {"type": "multiple", "difficulty": "easy", "category": "Science: Computers",
+     "question": "HTML is what type of language?", "correct_answer": "Markup Language",
+     "incorrect_answers": ["Macro Language", "Programming Language", "Scripting Language"]},
+    {"type": "multiple", "difficulty": "hard", "category": "History",
+     "question": "What was found in 1946 by two young shepards near a cave?", "correct_answer": "Dead Sea Scrolls",
+     "incorrect_answers": ["The Blackbeard Chest", "Sheep", "The First Oaxaca Cave Sleeper"]},
+    {"type": "multiple", "difficulty": "hard", "category": "Entertainment: Music",
+     "question": "Who was the original drummer for The Beatles?", "correct_answer": "Tommy Moore",
+     "incorrect_answers": ["Ringo Starr", "Stuart Sutcliffe", "Pete Best"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "AMD created the first consumer 64-bit processor.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"type": "multiple", "difficulty": "hard", "category": "Entertainment: Television",
+     "question": "Which country does the YouTuber &quot;SinowBeats&quot; originate from?", "correct_answer": "Scotland",
+     "incorrect_answers": ["England", "Sweden", "Germany"]},
+    {"type": "multiple", "difficulty": "medium", "category": "Entertainment: Video Games",
+     "question": "What was the name of the first MMORPG to popularize the genre?", "correct_answer": "Ultima Online",
+     "incorrect_answers": ["World of Warcraft", "Meridian 59", "Guild Wars"]},
+    {"type": "multiple", "difficulty": "hard", "category": "Mythology",
+     "question": "Which Norse God has a horse named Sleipnir?", "correct_answer": "Odin",
+     "incorrect_answers": ["Thor", "Frigg", "Balder"]}]
